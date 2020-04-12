2c1df809b528df57d44dbd4531e05c42bb64e2ee
madhuvayya <madhu.vayya@gmail.com>
2020-04-13 01:14:48 +0530

    Added codition for resign for the day

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -3,18 +3,26 @@
 STAKE=100
 BET=1
 
-cash=$STAKE
 
 while true
 do
-	res=$((RANDOM%2))
+	cash=$STAKE
 
-	if [ $res -eq 1 ]
-	then
-		cash=$(($cash+$BET))
-	else
-		cash=$(($cash-$BET))
-	fi
+	while true
+	do
+		if [ $cash -eq $(($STAKE/2)) -o $cash -eq $(($STAKE+$STAKE/2)) ]
+		then
+			echo $cash
+			break
+		fi
 
-	echo $cash
+		res=$((RANDOM%2))
+
+		if [ $res -eq 1 ]
+		then
+			cash=$(($cash+$BET))
+		else
+			cash=$(($cash-$BET))
+		fi
+	done
 done
