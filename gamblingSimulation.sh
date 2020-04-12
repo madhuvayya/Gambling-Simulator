3255d06938f2c2681ddb0e2d8f1940c32e145c30
madhuvayya <madhu.vayya@gmail.com>
2020-04-13 00:44:32 +0530

    Added betting operation

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,4 +1,20 @@
 #!/bin/bash -x
 
-stake=100
-bet=0
+STAKE=100
+BET=1
+
+cash=$STAKE
+
+while true
+do
+	res=$((RANDOM%2))
+
+	if [ $res -eq 1 ]
+	then
+		cash=$(($cash+$BET))
+	else
+		cash=$(($cash-$BET))
+	fi
+
+	echo $cash
+done
