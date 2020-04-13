c8315a67931ca6a3579859a1d11d659609cbd12f
madhuvayya <madhu.vayya@gmail.com>
2020-04-13 10:18:27 +0530

    Added if statement to continue gambling or to quit for next month

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 STAKE=100
 BET=1
@@ -7,39 +7,10 @@ WON=150
 LOST=50
 
 numDays=0
-totalAmount=0
+monthEarnedAmount=0
 wonDays=0
 lostDays=0
-
-i=0
-while [ $numDays -lt $TOTAL_DAYS ]
-do
-	cash=$STAKE
-	((numDays++))
-
-	while true
-	do
-		if [ $cash -eq $(($STAKE/2)) -o $cash -eq $(($STAKE+$STAKE/2)) ]
-		then
-			echo $cash
-			break
-		fi
-
-		res=$((RANDOM%2))
-
-		if [ $res -eq 1 ]
-		then
-			cash=$(($cash+$BET))
-		else
-			cash=$(($cash-$BET))
-		fi
-	done
-
-	dayWonLost[((i++))]=$cash
-	totalAmount=$(($totalAmount+$cash))
-done
-
-echo $totalAmount
+diffAmount=0
 
 daysWonLost(){
 	for((k=0;k<${#dayWonLost[@]};k++))
@@ -57,13 +28,13 @@ daysWonLost(){
 	echo "Number of days won: $wonDays"
 	echo "Number of days lost: $lostDays"
 
-	amount=$(($totalAmountMonth-$(($STAKE*$TOTAL_DAYS))))
+	diffAmount=$(($totalAmountMonth-$(($STAKE*$TOTAL_DAYS))))
 
-	if [ $amount -gt $(($STAKE*$TOTAL_DAYS)) ]
+	if [ $diffAmount -gt $(($STAKE*$TOTAL_DAYS)) ]
 	then
-		echo "Won the month with amount: $amount"
+		echo "Won the month with amount: $diffAmount"
 	else
-		echo "Lost the month with amount: $amount"
+		echo "Lost the month with amount: $diffAmount"
 	fi
 }
 
@@ -89,6 +60,45 @@ luckiestUnluckiestDay(){
 	echo "Unluckiest day: $min"
 }
 
-daysWonLost
+while true
+do
+	i=0
+	while [ $numDays -lt $TOTAL_DAYS ]
+	do
+		cash=$STAKE
+		((numDays++))
 
-luckiestUnluckiestDay
+		while true
+		do
+			if [ $cash -eq $(($STAKE/2)) -o $cash -eq $(($STAKE+$STAKE/2)) ]
+			then
+				#echo $cash
+				break
+			fi
+
+			res=$((RANDOM%2))
+
+			if [ $res -eq 1 ]
+			then
+				cash=$(($cash+$BET))
+			else
+				cash=$(($cash-$BET))
+			fi
+		done
+
+		dayWonLost[((i++))]=$cash
+		monthEarnedAmount=$(($monthEarnedAmount+$cash))
+	done
+
+	echo "Amount earned in month: $monthEarnedAmount"
+
+	daysWonLost
+
+	luckiestUnluckiestDay
+
+	if [ $diffAmount -lt $(($STAKE*$TOTAL_DAYS)) ]
+	then
+		echo "Lost this month so not going to play next month"
+		exit
+	fi
+done
